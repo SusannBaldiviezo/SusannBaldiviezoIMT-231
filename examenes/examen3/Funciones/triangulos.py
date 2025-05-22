b16435fa85736f92022fa492ef1e09d4de4ccc25
SusannBaldiviezo <susann.baldiviezo@ucb.edu.bo>
2025-05-22 12:34:07 -0400

    Func triangulos

---
+++ examenes/examen3/Funciones/triangulos.py
@@ -0,0 +1,8 @@
+def triangulo(n):
+    alfabeto = [
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+        'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+    ]
+    for i in range(n):
+        letra = alfabeto[i % 26]
+        print(letra * (i + 1))
